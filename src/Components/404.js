e517b089609e5d3c1bad99e8771511a6bf5b9e31
M Surjith Kumar <surjith.kumar@sifycorp.com>
2024-02-14 10:07:50 +0530

    chagnes

--- src/Components/404.js
+++ src/Components/404.js
@@ -25,7 +25,7 @@ function Notfound(){
                             <h1 className="display-1">404</h1>
                             <h1 className="mb-4">Page Not Found</h1>
                             <p className="mb-4">We’re sorry, the page you have looked for does not exist in our website! Maybe go to our home page or try to use a search?</p>
-                            <a className="btn btn-primary rounded-pill py-3 px-5" href="">Go Back To Home</a>
+                            <a className="btn btn-primary rounded-pill py-3 px-5" href="/">Go Back To Home</a>
                         </div>
                     </div>
                 </div>
